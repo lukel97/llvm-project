ac48bcf4815a5ac0c7743c8f68c8082eed6cc633
Luke Lau <luke@igalia.com>
2025-01-06 19:17:08 +0800

    Use dyn_cast

--- llvm/lib/Transforms/Vectorize/VPlan.cpp
+++ llvm/lib/Transforms/Vectorize/VPlan.cpp
@@ -1029,8 +1029,7 @@ void VPlan::execute(VPTransformState *State) {
     if (isa<VPWidenPHIRecipe>(&R))
       continue;
 
-    if (isa<VPWidenPointerInductionRecipe>(&R)) {
-      auto *WidenPhi = cast<VPWidenPointerInductionRecipe>(&R);
+    if (auto *WidenPhi = dyn_cast<VPWidenPointerInductionRecipe>(&R)) {
       assert(!WidenPhi->onlyScalarsGenerated(State->VF.isScalable()) &&
              "recipe generating only scalars should have been replaced");
       auto *GEP = cast<GetElementPtrInst>(State->get(WidenPhi));
